68ad8292ae76661b171bc8c1e3acbf962fceb833
ahmed <ahmedlearn01@gmail.com>
2021-11-05 12:54:25 -0700

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,2 +1,11 @@
 import firebase from "firebase/app"
 import "firebase/auth";
+
+export default auth firebase.initializeApp({
+    apiKey: "AIzaSyADxn41ouUhIglwlL1KQWvXR7gNQUWCo-A",
+    authDomain: "unichat-cbfd8.firebaseapp.com",
+    projectId: "unichat-cbfd8",
+    storageBucket: "unichat-cbfd8.appspot.com",
+    messagingSenderId: "1006324371983",
+    appId: "1:1006324371983:web:6a8b22ae054a29d470c97a"
+  }).auth();
