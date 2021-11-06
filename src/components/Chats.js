573f920fb317ad0512a4bcdb761db96183ba9e52
ahmed <ahmedlearn01@gmail.com>
2021-11-06 07:47:39 -0700

    initial commit

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useHistory } from 'react-router';
+import {ChatEngine} from 'react-chat-engine';
+import {auth} from '../firebase';
 
 const Chats = () =>{
     return (
-        <h1>Chats</h1>
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo">
+                    Unichat
+                </div>
+                <div className="logout-tab">
+                    Logout
+                </div>
+            </div>
+
+            <ChatEngine 
+                height="calc(100vh - 66px)"
+                projectId="1d0b9a90-4126-4f6c-a369-c8927ae6928d"
+                userName="."
+                userSecret="."
+            />
+        </div>
     )
 }
 
