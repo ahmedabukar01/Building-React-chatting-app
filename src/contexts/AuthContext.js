a9b884c66b05b721104a19b12b5514c39c8a05c8
ahmed <ahmedlearn01@gmail.com>
2021-11-06 07:22:36 -0700

    context hooks

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -7,5 +7,17 @@ const AuthContext = React.createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({children}) =>{
-    
+    const [user,setUser] = useState({});
+    const [loading, setLoading] = useState(true);
+    const history = useHistory();
+
+    useEffect(()=>{
+        auth.onAuthStateChanged((user) =>{
+            setUser(user);
+            setLoading(false);
+            history.push('/chats');
+        })
+    },[user,history]);
+
+    const 
 }
