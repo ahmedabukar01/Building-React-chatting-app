afddd771e7708359ef5e84f777be16f64ba8a161
ahmed <ahmedlearn01@gmail.com>
2021-11-07 08:31:06 -0800

    initial commit

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import {useRef, useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import {ChatEngine} from 'react-chat-engine';
 import {auth} from '../firebase';
 import { useAuth } from '../contexts/AuthContext';
+import axios from 'axios';
 
 const Chats = () =>{
     const history = useHistory();
     const { user } = useAuth();
-    console.log(user)
+    
+    useEffect(()=>{
+        if(!user){
+            history.push('/');
+            return;
+        }
+
+        axios.get('https://api.chatengine.io/users/me',{
+            headers: {
+                "project-id": "25549201-c7a1-4386-8afc-bb95c40bd246",
+                "user-name": user.email,
+                "user-secret": user.uid,
+            }
+        })
+    },[user,history])
 
     const handleLogout = async ()=>{
         await auth.signOut();
